cfe595c2f92afde3eeb18673a120abce4070aa89
Luiz Brandão <luizao.cmd@gmail.com>
2013-05-28 22:03:49 -0400

    Refatorando funcao showValue

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 function showValue(url){
 	var frame = document.getElementById("mainInfoFrame");
 	document.getElementById('info').style.display = 'none';
-	frame.src = url;
+	frame.src = 'http://en.m.wikipedia.org/wiki/'+url;
 }
